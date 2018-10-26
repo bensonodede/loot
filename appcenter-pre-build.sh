d4fc7bf05025606adf76293b7f05d0fff37738fd
bensonodede <bensonodede@gmail.com>
2018-10-26 18:49:24 +0300

    appcenter bash script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -5,7 +5,7 @@ APP_CONSTANT_FILE=$APPCENTER_SOURCE_DIRECTORY
 if [ "$APPCENTER_BRANCH" == "master" ];
 then 
     echo "Checking app directory..."
-    cat APP_CONSTANT_FILE
+    cat $APP_CONSTANT_FILE
 
 fi
 #sed -i 's/storeFile file(project.env.get("MYAPP_RELEASE_STORE_FILE"))/${System.env.MYAPP_RELEASE_STORE_FILE}/g' 
