8213e7febc745f43b3433cd1a7fd9c6d92f82d81
bensonodede <bensonodede@gmail.com>
2018-10-26 20:16:03 +0300

    appcenter post-clone bash script

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -6,8 +6,8 @@ echo "Current branch is $APPCENTER_BRANCH"
 if [ "$APPCENTER_BRANCH" == "master" ];
 then 
     echo "Current branch is $APPCENTER_BRANCH"
+    #sed -i 's/storeFile file(project.env.get("MYAPP_RELEASE_STORE_FILE"))/${System.env.MYAPP_RELEASE_STORE_FILE}/g' 
     echo "Checking app directory"
     cat "$APP_CONSTANT_FILE"
 
 fi
-#sed -i 's/storeFile file(project.env.get("MYAPP_RELEASE_STORE_FILE"))/${System.env.MYAPP_RELEASE_STORE_FILE}/g' 
