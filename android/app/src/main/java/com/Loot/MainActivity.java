09a5592fb7f237513165fe896fc6979bfa3d7cbb
bensonodede <bensonodede@gmail.com>
2018-10-29 21:35:16 +0300

    Fullscreen splashscreen

--- android/app/src/main/java/com/Loot/MainActivity.java
+++ android/app/src/main/java/com/Loot/MainActivity.java
@@ -3,12 +3,14 @@ package com.Loot;
 import com.facebook.react.ReactActivity;
 import com.reactnativecomponent.splashscreen.RCTSplashScreen;
 import android.os.Bundle; // required for onCreate parameter
-//import android.widget.ImageView; // uncomment if opening fullscreen
+import android.widget.ImageView; // uncomment if opening fullscreen
 
 public class MainActivity extends ReactActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        RCTSplashScreen.openSplashScreen(this); // open splashscreen
+        //RCTSplashScreen.openSplashScreen(this); // open splashscreen
+        RCTSplashScreen.openSplashScreen(this, true, ImageView.ScaleType.CENTER_INSIDE);   //open splashscreen fullscreen, use CENTER_INSIDE instead of FIT_XY to maintain aspect ratio
+
         // RCTSplashScreen.openSplashScreen(this, true, ImageView.ScaleType.FIT_XY);
         // //open splashscreen fullscreen
         super.onCreate(savedInstanceState);
